527ade182c4310ba4aecec5f63c85aee9a15513b
이지학 <jooin2000@naver.com>
2019-07-31 02:42:15 +0900

    Developing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
 import React,{ Component} from 'react';
 import './App.css';
-
-
+class Name extends Component {
+  state ={
+    name: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+  render(){
+    return(
+      <form>
+        <input
+        placeholder="조 이름 입력"
+        value={this.state.name}
+        onChange={this.handleChange}
+        />
+      </form>
+    )
+  }
+}
+class StudentNumber extends Component {
+  state ={
+    name: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+  render(){
+    return(
+      <form>
+        <input
+        placeholder="학번 입력"
+        value={this.state.name}
+        onChange={this.handleChange}
+        />
+      </form>
+    )
+  }
+}
+class PhoneForm extends Component {
+  state = {
+    name: '',
+    phone: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    // 페이지 리로딩 방지
+    e.preventDefault();
+    // 상태값을 onCreate 를 통하여 부모에게 전달
+    this.props.onCreate(this.state);
+    // 상태 초기화
+    this.setState({
+      name: '',
+      phone: ''
+    })
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          placeholder="이름"
+          value={this.state.name}
+          onChange={this.handleChange}
+          name="name"
+        />
+        <input
+          placeholder="학번"
+          value={this.state.phone}
+          onChange={this.handleChange}
+          name="phone"
+        />
+        <button type="submit">초대하기</button>
+      </form>
+    );
+  }
+}
 class App extends Component {
   render(){
   return (
       <div className="App">
         <header className="App-header">
           <h1>
-           <a href="">초대하기</a>
+            
+           조 이름 <Name/> 
+           <button>설정</button><button type="button" class="btn btn-outline-primary">변경</button>
           </h1>
         </header>
         <nav>
           <h2>
-            초대받은 내역
+            초대하기
+            <PhoneForm/> 
           </h2>
         </nav>
         <article>
-          <li>from 이지학 님 <button>수락</button><button>거절</button></li>
-          <li>from 신동민 님 <button>수락</button><button>거절</button></li>
+            <h1>
+              초대내역
+              <li>
+                보냄
+                
+              </li>
+            </h1>
         </article>
+
       </div>
     );
   }
